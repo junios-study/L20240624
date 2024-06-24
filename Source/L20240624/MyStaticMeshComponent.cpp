775023c500e6c55ee8bcc42cbfb496da1aaab2ac
junios <junios@naver.com>
2024-06-24 15:40:03 +0900

    TickComponent 실행

--- Source/L20240624/MyStaticMeshComponent.cpp
+++ Source/L20240624/MyStaticMeshComponent.cpp
@@ -5,14 +5,14 @@
 
 UMyStaticMeshComponent::UMyStaticMeshComponent()
 {
-
+	PrimaryComponentTick.bCanEverTick = true;
 }
 
 void UMyStaticMeshComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	AddLocalRotation((FRotator(0, 0, 3600.f * DeltaTime)));
+	AddLocalRotation(FRotator(0, 0, 3600.f * DeltaTime));
 }
 
 
