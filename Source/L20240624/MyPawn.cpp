1f1476fd764234ba21e8e5e4eef393416d04f7c3
junios <junios@naver.com>
2024-06-24 14:57:08 +0900

    움직이기 값 조정

--- Source/L20240624/MyPawn.cpp
+++ Source/L20240624/MyPawn.cpp
@@ -19,9 +19,16 @@ AMyPawn::AMyPawn()
 
 	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
 	RootComponent = Box;
+	Box->SetBoxExtent(FVector(45.f, 65.f, 15.f));
 
 	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
 	Body->SetupAttachment(Box);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Body(TEXT("/Script/Engine.StaticMesh'/Game/My/Meshes/SM_P38_Body.SM_P38_Body'"));
+	if (SM_Body.Succeeded())
+	{
+		Body->SetStaticMesh(SM_Body.Object);
+	}
+	
 
 	Left = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left"));
 	Left->SetupAttachment(Body);
@@ -29,16 +36,32 @@ AMyPawn::AMyPawn()
 	Right = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Right"));
 	Right->SetupAttachment(Body);
 
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Propeller(TEXT("/Script/Engine.StaticMesh'/Game/My/Meshes/SM_P38_Propeller.SM_P38_Propeller'"));
+	if (SM_Propeller.Succeeded())
+	{
+		Left->SetStaticMesh(SM_Propeller.Object);
+		Right->SetStaticMesh(SM_Propeller.Object);
+	}
+	Left->SetRelativeLocation(FVector(36.5f, 21.0f, 0.5f));
+	Right->SetRelativeLocation(FVector(36.5f, -21.0f, 0.5f));
+
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArm->SetupAttachment(Box);
+	SpringArm->TargetArmLength = 100.0f;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->bEnableCameraRotationLag = true;
+
 
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm);
 
 	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
 	Arrow->SetupAttachment(Box);
+	Arrow->SetRelativeLocation(FVector(80.0f, 0, 0));
 
 	Movement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Movement"));
+
+	Booster = 1.0f;
 }
 
 // Called when the game starts or when spawned
@@ -53,6 +76,8 @@ void AMyPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	AddMovementInput(GetActorForwardVector(),
+		Booster);
 }
 
 // Called to bind functionality to input
